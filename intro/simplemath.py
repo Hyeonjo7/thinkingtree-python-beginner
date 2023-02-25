ebd089baa0c3d7d9c9890220bed6bd2a55eae377
Eric <erichklee43@gmail.com>
2023-02-25 15:26:41 +1100

    added a simplemath file

---
+++ intro/simplemath.py
@@ -0,0 +1,20 @@
+# it is printing the math calculation
+print(2+2) 
+
+# it prints everything in ""
+print("2+2")
+
+# "" = string
+# string is a bunch of characters that 
+# aren't supposed to be calculated
+
+# example of integer and string not adding
+# print(1 + "1")
+
+print (10-2)
+
+# [shift 8] - the sharp symbol is a multiplication symbol
+print (12*10) 
+
+# the symbol: / is for dividing
+print (20/10)

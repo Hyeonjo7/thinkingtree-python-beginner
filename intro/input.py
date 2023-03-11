4a87c1c996c9d966dcce5bd6257adb17e1ed194f
Eric <erichklee43@gmail.com>
2023-03-11 15:14:16 +1100

    created input.py

---
+++ intro/input.py
@@ -0,0 +1,13 @@
+# program description:
+# ask for character name, age, strengths, weaknesses,
+# then print everything
+
+character_name = input("What is your character called? ")
+age = input("What's your character's age? ")
+strengths = input("What's your character's strength? ")
+weaknesses = input("What's your character's weakness? ")
+
+print("Your character's name is", character_name)
+print("Your character's age is ", age)
+print("Your character's strength is ", strength)
+print("Your character's weakness is ", weakness)
